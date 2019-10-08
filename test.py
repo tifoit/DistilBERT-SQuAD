37b91576c233ab423c901347348593b1793b51fe
Oliver Proud <owproud@gmail.com>
2019-10-08 13:30:44 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ from model import Model
 
 model = Model('model')
 
-context = "Netflix uses a variety of methods to help you find TV shows and movies to enjoy. You can find TV shows and movies through Recommendations or Search, or by browsing through categories."
+context = "Netflix uses a variety of methods to help you find TV shows and movies to enjoy. \
+        You can find TV shows and movies through Recommendations or Search, or by browsing through categories."
 
 question = "How do I find TV shows and movies on Netflix?"
 
