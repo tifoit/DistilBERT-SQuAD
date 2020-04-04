0f7046477afa7e2f8d09dbf3f83280fcf18b9da6
Oliver Proud <owproud@gmail.com>
2020-04-04 19:56:22 +0100

    cleanup download.py

--- download.py
+++ download.py
@@ -3,8 +3,6 @@ import requests
 import os
 
 
-s3_model_url = 'https://distilbert-finetuned-model.s3.eu-west-2.amazonaws.com/pytorch_model.bin'
-
 def download_model(s3_url, model_name):
     path = "./model"
     path_to_model = os.path.join(path, model_name)
@@ -16,6 +14,4 @@ def download_model(s3_url, model_name):
         r = requests.get(s3_url)
         filename.write_bytes(r.content)
 
-    return path_to_model, path
-
-path_to_model, path = download_model(s3_model_url, model_name="pytorch_model.bin")
+    return path_to_model
