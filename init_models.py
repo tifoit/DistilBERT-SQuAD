886d5d190f1f8dedb267353c47aa37b39bf18a44
Ricardo Leon <ricardo.leon@pureinsights.com>
2022-03-17 11:10:19 -0600

    OP-42 Removing Oliver Proud's model

--- init_models.py
+++ init_models.py
@@ -4,7 +4,6 @@ NOTE: This file is only meant to run locally to download models!
 import model
 
 models = [{"model": 'deepset/roberta-base-squad2'},
-          {"model": 'oliverproud/distilbert-finetuned-model'},
           {"model": 'mrm8488/distilbert-multi-finetuned-for-xqua-on-tydiqa'},
           {"model": 'distilbert-base-cased-distilled-squad'}]
 path = "./models"
